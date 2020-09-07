53bc6530a9e32d84c86368ede5ebbcabfa4c471d
ksu <ksuhablondo@gmail.com>
2020-09-07 13:32:14 +0300

    Updates

---
+++ precache-manifest.08a521e9da2262bd0e723c3130c8b931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d5016a4a3e6097f78d53b2d6f082f9",
+    "url": "/productList/index.html"
+  },
+  {
+    "revision": "3e9b698a6e75e0acf1b5",
+    "url": "/productList/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "60ecb8af1acbc55d1d05",
+    "url": "/productList/static/css/main.d2d768ce.chunk.css"
+  },
+  {
+    "revision": "3e9b698a6e75e0acf1b5",
+    "url": "/productList/static/js/2.189ef55d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/productList/static/js/2.189ef55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ecb8af1acbc55d1d05",
+    "url": "/productList/static/js/main.ea655355.chunk.js"
+  },
+  {
+    "revision": "4ef75692d4d7836898f3",
+    "url": "/productList/static/js/runtime-main.10d689c2.js"
+  }
+]);
